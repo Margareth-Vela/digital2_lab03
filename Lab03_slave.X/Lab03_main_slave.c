24ea63ddcb6e384baf06e4066b4e1e3bdbc90db3
Margareth-Vela <68076747+Margareth-Vela@users.noreply.github.com>
2021-08-02 21:56:47 -0600

    Circuito y configuraciones iniciales del pic master y slave

---
+++ Lab03_slave.X/Lab03_main_slave.c
@@ -0,0 +1,108 @@
+/*
+ * Archivo:   Lab03_main_master.c
+ * Dispositivo: PIC16F887
+ * Autor: Margareth Vela 
+ * 
+ * Programa: SPI
+ * Hardware: 
+ * 
+ * Creado: Agosto 02, 2021
+ * Última modificación: Agosto, 2021
+ */
+
+//------------------------------------------------------------------------------
+//                          Importación de librerías
+//------------------------------------------------------------------------------    
+ #include <xc.h>
+ #include <stdint.h>
+ #include "SPI.h"
+
+//------------------------------------------------------------------------------
+//                          Directivas del compilador
+//------------------------------------------------------------------------------
+#define _XTAL_FREQ 8000000 //Para delay
+
+//------------------------------------------------------------------------------
+//                          Palabras de configuración
+//------------------------------------------------------------------------------    
+// CONFIG1
+#pragma config FOSC = INTRC_NOCLKOUT//Oscillator Selection bits(INTOSCIO 
+                              //oscillator: I/O function on RA6/OSC2/CLKOUT pin, 
+                              //I/O function on RA7/OSC1/CLKIN)
+#pragma config WDTE = OFF // Watchdog Timer Enable bit (WDT disabled and 
+                          //can be enabled by SWDTEN bit of the WDTCON register)
+#pragma config PWRTE = ON       // Power-up Timer Enable bit (PWRT enabled)
+#pragma config MCLRE = OFF      // RE3/MCLR pin function select bit (RE3/MCLR  
+                                //pin function is digital input, MCLR internally 
+                                //tied to VDD)
+#pragma config CP = OFF         // Code Protection bit (Program memory code 
+                                //protection is disabled)
+#pragma config CPD = OFF        // Data Code Protection bit (Data memory code 
+                                //protection is disabled)
+#pragma config BOREN = OFF      // Brown Out Reset Selection bits (BOR disabled)
+#pragma config IESO = OFF       // Internal External Switchover bit 
+                                //Internal/External Switchover mode is disabled
+#pragma config FCMEN = OFF      // Fail-Safe Clock Monitor Enabled bit 
+                                //(Fail-Safe Clock Monitor is disabled)
+#pragma config LVP = OFF         //Low Voltage Programming Enable bit(RB3/PGM pin 
+                                //has PGM function, low voltage programming 
+                                //enabled)
+// CONFIG2
+#pragma config BOR4V = BOR40V   // Brown-out Reset Selection bit (Brown-out 
+                                //Reset set to 4.0V)
+#pragma config WRT = OFF        // Flash Program Memory Self Write Enable bits 
+                                //(Write protection off)
+
+//------------------------------------------------------------------------------
+//                          Variables
+//------------------------------------------------------------------------------
+uint8_t temporal = 0;
+
+
+//------------------------------------------------------------------------------
+//                          Prototipos
+//------------------------------------------------------------------------------
+void setup(void);  //Configuración
+
+//------------------------------------------------------------------------------
+//                          Interrupciones
+//------------------------------------------------------------------------------
+void __interrupt() isr(void){
+   if(SSPIF == 1){
+        PORTD = spiRead();
+        spiWrite(PORTB);
+        SSPIF = 0;
+    }
+}
+//------------------------------------------------------------------------------
+//                          Código Principal
+//------------------------------------------------------------------------------
+void main(void) {
+    setup();
+    while(1){
+       PORTB--;
+       __delay_ms(250);
+    }
+    return;
+}
+//------------------------------------------------------------------------------
+//                          Configuración
+//------------------------------------------------------------------------------
+void setup(void){
+    ANSEL = 0;
+    ANSELH = 0;
+    
+    TRISB = 0;
+    TRISD = 0;
+    
+    PORTB = 0;
+    PORTD = 0;
+    
+    INTCONbits.GIE = 1;         // Habilitamos interrupciones
+    INTCONbits.PEIE = 1;        // Habilitamos interrupciones PEIE
+    PIR1bits.SSPIF = 0;         // Borramos bandera interrupción MSSP
+    PIE1bits.SSPIE = 1;         // Habilitamos interrupción MSSP
+    TRISAbits.TRISA5 = 1;       // Slave Select
+    spiInit(SPI_SLAVE_SS_EN, SPI_DATA_SAMPLE_MIDDLE, SPI_CLOCK_IDLE_LOW, SPI_IDLE_2_ACTIVE);
+   
+}
---
+++ Lab03_slave.X/build/default/production/Lab03_main_slave.p1.d
@@ -0,0 +1,3 @@
+build/default/production/Lab03_main_slave.p1:  \
+Lab03_main_slave.c  \
+SPI.h 
